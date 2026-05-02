a3ddb90364e33dbdf54f9ac9f650eb576483595e
Ayanfeoluwa <aamadewoyin@gmail.com>
2026-05-02 23:56:03 +0100

    feat: expose Inngest API route for user synchronization functions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,8 @@
 import { serve } from 'inngest/next'
-import {innjest} from '../../../inngest/client'
+import {inngest} from '../../../inngest/client'
 import { syncUserCreation, syncUserUpdation, syncUserDeletion } from '@/inngest/functions'
 
 export const {GET, POST, PUT} = server({
-    client: innjest,
+    client: inngest,
     functions: [syncUserCreation, syncUserUpdation, syncUserDeletion]
 })
