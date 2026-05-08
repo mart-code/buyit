f8bdea3472820d927c9e6d3c6573f0fb3bd5f033
Ayanfeoluwa <aamadewoyin@gmail.com>
2026-05-08 15:56:59 +0100

    feat: implement Inngest functions to synchronize Clerk user events with Prisma database

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -7,6 +7,19 @@ import {
 } from "@/inngest/functions";
 
 
+await inngest.send({
+  // The event name
+  name: "clerk/user.created",
+  // The event's data
+  data: {
+    id: "ed12c8bde",
+    itemIds: ["9f08sdh84", "sdf098487", "0fnun498n"],
+    account: {
+      id: 123,
+      email: "[EMAIL_ADDRESS]",
+    },
+  },
+});
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
